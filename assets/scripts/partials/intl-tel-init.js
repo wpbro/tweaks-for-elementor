0fdd9306ef56f4bbec491cd54cdde6a4652b020c
Alexey Savkin <savkinshot@gmail.com>
2020-10-15 10:18:01 +0300

    feat--plugin-intl--library for elementor form country dial code added

--- assets/scripts/partials/intl-tel-init.js
+++ assets/scripts/partials/intl-tel-init.js
@@ -12,14 +12,19 @@ export default function () {
 
 	//This function init intl tel for each type=tel fields
 	const intlInit = (field, country) => {
-		intlTelInput(field, {
-			separateDialCode: true,
+		const iti = intlTelInput(field, {
+			separateDialCode: false,
 			nationalMode: false,
 			initialCountry: country,
-			preferredCountries: [],
 			utilsScript: utils,
 			autoPlaceholder: 'aggressive',
-			placeholderNumberType: 'FIXED_LINE',
+			placeholderNumberType: 'FIXED_LINE'
 		});
+
+		field.addEventListener('input', () => handleChange(iti, field));
+	}
+
+	function handleChange(iti, input) {
+		input.value = iti.getNumber();
 	}
 }
